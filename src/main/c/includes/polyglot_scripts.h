74d6d190c14c9e589cc62b745b23ef5ab28ab465
Kevin Menard <kevin@nirvdrum.com>
2022-03-16 16:24:18 -0400

    Move constant definitions into the Truffle language Haversine distance implementations to avoid conflicts re-evaluating the code snippet within the same context.
    We want to use one context for all benchmark runs, but the Google Benchmark runner will execute the benchmark function (and its setup) multiple times during an benchmark run. That ends up evaluating the snippet multiple times. Since we're using anonymous functions for the implementation, there are no conflicts with the distance methods, but trying to redefine a constant is porblematic.

--- src/main/c/includes/polyglot_scripts.h
+++ src/main/c/includes/polyglot_scripts.h
@@ -2,8 +2,8 @@
 #define __POLYGLOT_SCRIPTS_H
 
 const char* JS_HAVERSINE_DISTANCE =
-  "const EARTH_RADIUS = 6371;\n"
   "(a_lat, a_long, b_lat, b_long) => {\n"
+  "    const EARTH_RADIUS = 6371;\n"
   "    const a_lat_radians = a_lat * Math.PI / 180;\n"
   "    const a_long_radians = a_long * Math.PI / 180;\n"
   "    const b_lat_radians = b_lat * Math.PI / 180;\n"
@@ -16,7 +16,7 @@ const char* JS_HAVERSINE_DISTANCE =
   "}";
 
 const char* RUBY_HAVERSINE_DISTANCE =
-  "EARTH_RADIUS = 6371\n"
+  "EARTH_RADIUS = 6371 unless defined?(EARTH_RADIUS)\n"
   "->(a_lat, a_long, b_lat, b_long) do\n"
   "    a_lat_radians = a_lat * Math::PI / 180\n"
   "    a_long_radians = a_long * Math::PI / 180\n"
