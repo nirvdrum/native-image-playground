afd9a048affd1c9d995c2360745ece2dd4434fdb
Kevin Menard <kevin.menard@shopify.com>
2022-05-04 10:47:31 -0400

    Ensure everyone setting up the Google Benchmark library is using the same version.

--- build-google-benchmark.sh
+++ build-google-benchmark.sh
@@ -3,9 +3,10 @@
 export CXX=clang++
 
 if [[ ! -d benchmark ]]; then
-  git clone --depth 1 https://github.com/google/benchmark.git
+  git clone https://github.com/google/benchmark.git
 
   pushd benchmark
+  git checkout 8d86026c67e41b1f74e67c1b20cc8f73871bc76e
 
   cmake -E make_directory "build"
   cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
