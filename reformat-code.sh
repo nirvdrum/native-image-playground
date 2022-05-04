ac2c8c59fa275d7b311f4748ed37b6ae43fe21b0
Kevin Menard <kevin.menard@shopify.com>
2022-05-04 10:47:31 -0400

    Dumped the `clang-format` Google style to a local file for better tool interop.
    E.g., IDEs can read the .clang-format file to provide code formatting directly in the editor.

--- reformat-code.sh
+++ reformat-code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find src -type f -regex '.*\.\(cxx\|c\|h\)$' -print0 | xargs -0 -n1 clang-format -i --style=google --sort-includes
+find src -type f -regex '.*\.\(cxx\|c\|h\)$' -print0 | xargs -0 -n1 clang-format -i --style=file --sort-includes
