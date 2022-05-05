0e8e1d0e5bf4b38a8087ea519dddcdfaf45247c2
Kevin Menard <kevin@nirvdrum.com>
2022-05-04 20:50:39 -0400

    Add ShellCheck suggestion to exit the Google Benchmark setup script if `pushd` or `popd` fail.

--- build-google-benchmark.sh
+++ build-google-benchmark.sh
@@ -5,12 +5,12 @@ export CXX=clang++
 if [[ ! -d benchmark ]]; then
   git clone https://github.com/google/benchmark.git
 
-  pushd benchmark
+  pushd benchmark || { echo "'benchmark' library source location does not exist!"; exit 1; }
   git checkout 8d86026c67e41b1f74e67c1b20cc8f73871bc76e
 
   cmake -E make_directory "build"
   cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
   cmake --build "build" --config Release
 
-  popd
+  popd || { echo "Unable to return to parent directory!"; exit 1; }
 fi
