f8e44f4119bd393e7adcaeca8ad765bb290705bb
Kevin Menard <kevin@nirvdrum.com>
2022-05-04 20:50:39 -0400

    Make the Google Benchmark version to install easier to adjust.

--- build-google-benchmark.sh
+++ build-google-benchmark.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+BENCHMARK_VERSION=8d86026c67e41b1f74e67c1b20cc8f73871bc76e
 export CXX=clang++
 
 if [[ ! -d benchmark ]]; then
   git clone https://github.com/google/benchmark.git
 
   pushd benchmark || { echo "'benchmark' library source location does not exist!"; exit 1; }
-  git checkout 8d86026c67e41b1f74e67c1b20cc8f73871bc76e
+  git checkout $BENCHMARK_VERSION
 
   cmake -E make_directory "build"
   cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
