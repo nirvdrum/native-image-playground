e3cba35e6b8c629b3992aa99be4bbf519eb4048e
Kevin Menard <kevin@nirvdrum.com>
2022-08-02 12:56:09 -0400

    Update linter and code formatter work on both Linux and macOS.

--- reformat-code.sh
+++ reformat-code.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-find src -type f -regex '.*\.\(cxx\|c\|h\)$' -print0 | xargs -0 -n1 clang-format -i --style=file --sort-includes
+os=$(uname)
+find_pattern='.*\.(cxx|c|h)$'
+
+if [ "$os" == 'Darwin' ]; then
+  find_command="find -E src -type f -regex '$find_pattern' -print0"
+else
+  find_command="find src -type f -regextype egrep -regex '$find_pattern' -print0"
+fi
+
+eval "$find_command" | xargs -0 -n1 clang-format -i --style=file --sort-includes
+
